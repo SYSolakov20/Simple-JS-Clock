54fe12609a5d1a4e888f028fcf7fd53b3ff68a96
SYSolakov20 <sysolakov20@codingburgas.bg>
2022-07-03 22:42:48 +0300

    fix clock

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 function showTime(){
     var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
     var session = "AM";
-    
-    if(h == 0){
+
+    if(hour == 0)
+    {
         h = 12;
     }
-    
-    if(h > 12){
-        h = h - 12;
+
+    if(hour > 12)
+    {
+        hour = hour -12;
         session = "PM";
     }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
+
+    hour = (hour < 10) ? "0" + hour : hour;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    var time = hour + ":" + minutes + ":" + seconds + " " + session;
+
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
-    
+
     setTimeout(showTime, 1000);
-    
+
 }
 
 showTime();
